5a371bcf5fde8cdbe16a3ca847991415ad150a99
rose-line <patrice.gahide@gmail.com>
2021-01-22 10:38:55 +0100

    first commit

---
+++ src/test/java/fr/dampierre/AppTest.java
@@ -0,0 +1,18 @@
+package fr.dampierre;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
